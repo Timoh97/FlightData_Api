dc2586c379fb65cfb0c8ce890756f0837cab010f
Timoh97 <timohmugendi@gmail.com>
2022-06-15 12:35:51 +0300

    set API variables

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
+from doctest import script_from_examples
 from flask import render_template
 from . import main
-from api import api_key
+from ..api import api_key
 
-import requests
+
+import json
 
 # url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/direct/"
 
@@ -18,6 +20,32 @@ import requests
 
 # print(response.text)
 
+
+
+
+
+import http.client
+
+conn = http.client.HTTPSConnection("airport-info.p.rapidapi.com")
+
+headers = {
+    'X-RapidAPI-Key': api_key,
+    'X-RapidAPI-Host': "airport-info.p.rapidapi.com"
+    }
+
+code='ABV'
+code1='/airport?iata'
+code3=f'{code1}={code}'
+conn.request("GET", code3 , headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+name=data.decode("utf-8")
+conv = json.loads(name)
+
 @main.route('/')
 def index():
-    return render_template("index.html")
+    airport={'airport':conv}
+    airport=airport['airport']
+    return render_template("index.html",airport=airport)
