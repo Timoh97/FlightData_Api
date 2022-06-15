3b2bbe03d085e23bdb735148ec5b5068f8d95349
Timoh97 <timohmugendi@gmail.com>
2022-06-15 15:25:55 +0300

    add API screenshots

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,15 @@
-from doctest import script_from_examples
+
 from flask import render_template
 from . import main
 from ..api import api_key
-
-
+import http.client
 import json
+
+
+
+
+
+
 
 # url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/direct/"
 
@@ -20,12 +25,6 @@ import json
 
 # print(response.text)
 
-
-
-
-
-import http.client
-
 conn = http.client.HTTPSConnection("airport-info.p.rapidapi.com")
 
 headers = {
@@ -33,7 +32,7 @@ headers = {
     'X-RapidAPI-Host': "airport-info.p.rapidapi.com"
     }
 
-code='ABV'
+code='ABI'
 code1='/airport?iata'
 code3=f'{code1}={code}'
 conn.request("GET", code3 , headers=headers)
@@ -43,6 +42,8 @@ data = res.read()
 
 name=data.decode("utf-8")
 conv = json.loads(name)
+
+
 
 @main.route('/')
 def index():
---
+++ app/static/file.js
@@ -0,0 +1,9 @@
+
+	function generateName(){
+		value1 =document.getElementById("month").value;
+	    value2=value1;
+        window.value3 = value2;
+        alert(value3)
+}
+
+value=value3
